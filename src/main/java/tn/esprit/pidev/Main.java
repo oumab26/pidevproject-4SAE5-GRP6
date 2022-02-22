27d0e1fffa0974a5b1c0518d15ee490d179228b9
oumab26 <balti.oumaima@esprit.tn>
2022-02-22 19:03:49 +0100

    finish crud 1er entity job offer

--- src/main/java/tn/esprit/pidev/Main.java
+++ src/main/java/tn/esprit/pidev/Main.java
@@ -2,8 +2,10 @@ package tn.esprit.pidev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+//@EnableWebMvc
+//@EnableScheduling
 @SpringBootApplication
 public class Main {
 
